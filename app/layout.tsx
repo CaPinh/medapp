a58f636f3a4714b739532b0dc1e71329bc5ac107
CaPinh <47351669+CaPinh@users.noreply.github.com>
2026-03-08 21:16:10 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  themeColor: '#16a34a',
+  themeColor: '#b21db5',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
